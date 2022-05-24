17a6fa3a4e3de112a507a70ced78376c85d9b9df
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-24 16:30:23 -0400

    validation for model : review/favorite/marker

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -4,4 +4,6 @@ class Marker < ApplicationRecord
   has_many :favorites
   has_one_attached :photo
 
+  validates :user, :title, :description, presence: true
+  validates :latitude, :longitude, uniqueness: true, presence: true
 end
