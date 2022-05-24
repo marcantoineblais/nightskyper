d021784187db3aa348fad99f2fcc97c89a994cd2
Marc-Antoine Blais <blaisma@live.fr>
2022-05-24 12:34:40 -0400

    added user skip check in page controller, added search and result methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[home result search]
 
   def home
+  end
+
+  def search
+  end
+
+  def result
   end
 end
