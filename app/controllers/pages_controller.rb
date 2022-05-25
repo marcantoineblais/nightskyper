dc09215bc59f3c03fcd7ba6a6c0a4bacc85cc160
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-25 17:42:50 -0400

    Add API call and read.json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
   def search
     if params[:query].present?
       search_by_address
+      load_weather_by_address(@center[0], @center[1])
       @map_markers = @markers.map { |marker| [marker.longitude, marker.latitude] }
     end
   end
@@ -27,5 +28,14 @@ class PagesController < ApplicationController
     bounds = doc['features'].first['bbox']
     @map_boundaries = bounds || [@center[0] - 0.022, @center[1] - 0.022, @center[0] + 0.022, @center[1] + 0.022]
     @markers = Marker.where('longitude > ? AND latitude > ? AND longitude < ? AND latitude < ?', *@map_boundaries)
+  end
+
+  def load_weather_by_address(longitude, latitude)
+    weather_url = "https://api.openweathermap.org/data/2.5/weather?appid=#{ENV['open_weather']}&lat=#{latitude}&lon=#{longitude}"
+    doc = JSON.parse(URI.open(weather_url).read)
+    weather_temp = doc['main']['temp']
+    @weather_descrip = doc['weather'].first['description']
+    @weather_humidity = doc['main']['humidity']
+    @temp_in_degre = ((weather_temp - 273.15) * 1.000000).round(2)
   end
 end
