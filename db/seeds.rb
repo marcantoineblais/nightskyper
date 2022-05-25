c8740d2cf543bb78f2cee556aca079c67b1a918b
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-25 10:43:14 -0400

    add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+puts "Users destroyed"
+Review.destroy_all
+puts "Reviews detroyed"
+Marker.destroy_all
+puts "Markers destroyed"
+Favorite.destroy_all
+puts "Favorites destroyed"
+
+a = User.create!(email: 'a@a.com', username: 'Atest', password: '123456')
+b = User.create!(email: 'b@b.com', username: 'Btest', password: '123456')
+c = User.create!(email: 'c@c.com', username: 'Ctest', password: '123456')
+
+# For the address :
+# Check la gem de Geocoding
+
+# http://api.positionstack.com/v1/forward?access_key=a5f3e8675d5ca91c470e9bbdcc288a62&country=CA&query=que
+# http://api.positionstack.com/v1/
