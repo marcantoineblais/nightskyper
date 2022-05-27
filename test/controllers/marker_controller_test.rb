f48a6d13852f15d7d29dfa8b3778a798822eba21
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-27 11:33:55 -0400

    Update marker_controller_test.rb

--- test/controllers/marker_controller_test.rb
+++ test/controllers/marker_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class MarkerControllerTest < ActionDispatch::IntegrationTest
+class MarkersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
