6bafd2e51face19b8205ade8540d57cb581bd02b
Marc-Antoine Blais <blaisma@live.fr>
2022-05-27 00:40:37 -0400

    added temporary cards to map, fixed bug when using browser back on page with map, overview and prediction are now being updated by map

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -6,49 +6,61 @@ export default class extends Controller {
   static values = {
     apiKey: String,
     searchPath: String,
-    bounds: Array,
-    center: Array
+    customMarkerPath: String,
+    bounds: Array
   }
 
   connect() {
-    console.log(document.getElementById('meteo'));
     mapboxgl.accessToken = this.apiKeyValue
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
+      pitch: 60,
+      bearing: -60,
     })
 
     window.addEventListener("load", () => {
       window.dispatchEvent(new Event('resize'))
-
-      if (this.boundsValue.length != 0) {
-        this.#fitMapToBoundaries()
       }
+    )
 
-      this.map.on('moveend', () => {
-        this.#getBoundariesCoordinates()
-        this.#updateMarkers()
-      })
+    this.#fitMapToBoundaries()
+    this.map.doubleClickZoom.disable()
+
+    this.map.on('moveend', () => {
+      this.#getBoundariesCoordinates()
+      this.#updateMarkers()
     })
 
-    this.map.on('click', (e) => {
-      this.centerValue = [e.lngLat.lng, e.lngLat.lat]
+    this.map.on('dblclick', (e) => {
+      const coordinates = [e.lngLat.lng, e.lngLat.lat]
+      fetch(this.customMarkerPathValue, {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Accept": "application/json", charset: "UTF-8" },
+        body: JSON.stringify({
+          coordinates: coordinates
+        })
+      })
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        if (document.querySelector('.search-marker')) {
+          document.querySelector('.search-marker').outerHTML = ""
+        }
 
-      if (document.querySelector('.search-marker')) {
-        document.querySelector('.search-marker').outerHTML = ""
-      }
+        this.#addMarkersToMap([data.customMarker], 'search-marker')
+        this.#recenterMapToBondaries(data.customMarker)
 
-      this.#addMarkersToMap([this.centerValue], 'search-marker')
-      this.#recenterMapToBondaries(this.centerValue)
+        if (document.getElementById('overview') && data.overview != document.getElementById('overview').outerHTML) {
+          document.getElementById('overview').outerHTML = data.overview
+        }
+      })
     })
   }
 
   #addMarkersToMap(markers, cssClass) {
     markers.forEach((marker) => {
       const markerDiv = document.createElement('div');
-      if (!marker.info_window) {
-        marker.info_window = ""
-      }
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       markerDiv.className = cssClass;
       new mapboxgl.Marker(markerDiv).setLngLat([marker.lon, marker.lat]).setPopup(popup).addTo(this.map)
@@ -75,23 +87,17 @@ export default class extends Controller {
       method: "POST",
       headers: { "Content-Type": "application/json", "Accept": "application/json", charset: "UTF-8" },
       body: JSON.stringify({
-        map_boundaries: this.boundsValue,
-        center: this.centerValue
+        map_boundaries: this.boundsValue
       })
     })
     .then(res => res.json())
     .then((data) => {
-      console.log(data)
       if (document.querySelectorAll('.pin-marker').length != 0) {
         document.querySelectorAll('.pin-marker').forEach((marker) => {
           marker.outerHTML=""
         })
       }
       this.#addMarkersToMap(data.mapMarkers, 'pin-marker')
-
-      if (document.getElementById('overview') && data.overview != document.getElementById('overview').outerHTML) {
-        document.getElementById('overview').outerHTML = data.overview
-      }
 
       if (document.getElementById('marker-cards') && data.markerCards != document.getElementById('marker-cards').outerHTML) {
         document.getElementById('marker-cards').outerHTML = data.markerCards
---
+++ test/controllers/marker_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MarkerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
