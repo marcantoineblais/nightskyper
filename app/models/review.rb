502c26b0b6926a3202059b53bb775ed5715ef23f
Marc-Antoine Blais <blaisma@live.fr>
2022-05-30 10:16:43 -0400

    added multiple photos for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :marker, dependent: :destroy
-  has_one_attached :photo
+  has_many_attached :photos
 
   validates :user, :marker, :rating, :content, :date, presence: true
   # validates :marker, uniqueness: { scope: :user, message: "Already left comment before" }
