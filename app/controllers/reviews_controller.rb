892d03d8a2833b59f3f780586164f71bcc9529d0
florent gilet <florent.gilet@protonmail.com>
2022-05-30 16:39:20 -0400

    Ready

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,6 +24,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :content, :date, :photos)
+    params.require(:review).permit(:rating, :content, :date, photos: [])
   end
 end
