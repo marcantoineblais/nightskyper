57465f52c9a1d9146859ccf515b3ca9df36f6b52
Marc-Antoine Blais <blaisma@live.fr>
2022-05-31 11:16:01 -0400

    migrated photo in users

---
+++ app/javascript/controllers/cloudinary_users_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    console.log('Hello, Stimulus!')
+
+    this.widget = cloudinary.applyUploadWidget(
+      this.element,
+      {
+        cloudName: 'diiu2j0fw',
+        uploadPreset: 'nightskyper',
+        cropping: true,
+        folder: 'users',
+        croppingAspectRatio: 1,
+        fieldName: 'user[photo]',
+        multiple: false
+      },
+      (error, result): {
+        console.log(error, result)
+      }
+    )
+  }
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_215736) do
+ActiveRecord::Schema.define(version: 2022_05_31_151533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_215736) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "photo"
     t.index ["user_id"], name: "index_markers_on_user_id"
   end
 
@@ -86,6 +87,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_215736) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
