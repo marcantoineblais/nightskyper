82f33aa53e709b90076572563023857b022d057f
Marc-Antoine Blais <blaisma@live.fr>
2022-06-02 10:49:40 -0400

    added pictures popup

---
+++ app/javascript/controllers/popup_img_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [
+    'modalImg',
+    'close',
+    'modal',
+    'img'
+  ]
+
+  connect() {
+    console.log("Bonjour / Welcome")
+    this.imgTarget.addEventListener('click', (e) => {
+      this.modalTarget.style.display = "block";
+      this.modalImgTarget.src = this.imgTarget.src;
+    })
+
+    // When the user clicks on <span> (x), close the modal
+    this.closeTarget.addEventListener('click', (e) => {
+      this.modalTarget.style.display = "none";
+    })
+  }
+}
